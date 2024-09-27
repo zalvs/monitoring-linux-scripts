13fa4702b1fde7dfceadd365842baed7f23890e1
zalvs <106460069+zalvs@users.noreply.github.com>
2024-09-26 21:19:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,8 @@ sudo apt install -y python3 python3-pip inotify-tools
 # Instalar pacotes Python necessários
 pip3 install inotify_simple
 
-# Tornar o script de monitoramento executável
-chmod +x scripts/monitor_unificado.py
+# Tornar o script de monitoramento executável (agora na raiz do projeto)
+chmod +x monitor_unificado.py
 
 echo "Instalação concluída! Para iniciar o monitoramento, execute o script:"
-echo "sudo python3 scripts/monitor_unificado.py"
-
+echo "sudo python3 monitor_unificado.py"
